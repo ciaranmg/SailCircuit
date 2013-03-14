f1583b82c98be79df680bc25e260ae1b90d07ff5
Ciaran McGrath <ciaranmg@gmail.com>
2013-03-14 20:22:56 +0000

    Added proper page titles to all pages

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><? if(isset($headline)) echo $headline; ?> SailCircut</title>
+    <title><? if(isset($headline)) echo $headline . ' - '; ?> <? if(isset($title)) echo $title . ' - '; ?> SailCircut</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="SailCircuit">
